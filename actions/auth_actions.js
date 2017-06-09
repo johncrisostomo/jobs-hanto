a10258a76d47e21a8d615d330c8380190a7555b6
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-09 11:31:46 +0800

    wraps up the facebook login action creator

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,14 +1,31 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
+import { appId } from '../facebookSettings.json';
 
 import {
   FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL,
 } from './types';
 
 export const facebookLogin = () => async dispatch => {
   let token = await AsyncStorage.getItem('fb_token');
+
   if (token) {
-
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
-
+    doFacebookLogin(dispatch);
   }
 }
+
+const doFacebookLogin = async dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync(appId, {
+    permission: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+};
