018a91e0f35c58380c6f63033e15d361d9eaa9e4
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-09 12:36:58 +0800

    cleans up auth screen

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -20,16 +20,7 @@ class AuthScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-      </View>
+      <View />
     )
   }
 }
