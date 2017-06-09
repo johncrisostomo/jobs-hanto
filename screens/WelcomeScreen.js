35bd8ae5c352a5abc3466dfb037b3332372517bc
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-09 09:44:06 +0800

    implements and styles the slides for the welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to Jobs Hanto', color: '#03A9F4' },
+  { text: 'Use this to find a job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4' },
+];
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
-    )
+      <Slides data={SLIDE_DATA} />
+    );
   }
 }
 
