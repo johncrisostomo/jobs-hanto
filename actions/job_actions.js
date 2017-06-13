3feb273f892851365d1f24ca47fad8e166f831ad
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-13 08:26:58 +0800

    fully implements job fetching action

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -8,6 +8,7 @@ import {
   FETCH_JOBS,
 } from './types';
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
   publisher,
   format: 'json',
@@ -17,9 +18,17 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript', // just hardcoding this for now
 };
 
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = region => async dispatch => {
   try {
     let zip = await reverseGeoCode(region);
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
   } catch(e) {
     console.error(e);
   }
