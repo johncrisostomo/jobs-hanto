b835cf49525df8e96884f184e40492486a557b50
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-13 08:19:02 +0800

    prepares zip code and query string for the API request

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
+import reverseGeoCode from 'lantlng-to-zip';
+import qs from 'qs';
+
+import { publisher } from '../indeedSettings.json';
 
 import {
   FETCH_JOBS,
 } from './types';
 
+const JOB_QUERY_PARAMS = {
+  publisher,
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript', // just hardcoding this for now
+};
+
 export const fetchJobs = region => async dispatch => {
-  
+  try {
+    let zip = await reverseGeoCode(region);
+  } catch(e) {
+    console.error(e);
+  }
 };
