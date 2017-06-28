6ac5dde430a0557fdf09a1e75bdb7f61ed208f3b
John Crisostomo <jd@snapzio.com>
2017-06-28 14:45:50 +0800

    implements the job liking logic

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,24 +1,22 @@
-import axios from 'axios';
-import reverseGeoCode from 'latlng-to-zip';
-import qs from 'qs';
+import axios from "axios";
+import reverseGeoCode from "latlng-to-zip";
+import qs from "qs";
 
-import { publisher } from '../indeedSettings.json';
+import { publisher } from "../indeedSettings.json";
 
-import {
-  FETCH_JOBS,
-} from './types';
+import { FETCH_JOBS, LIKE_JOB } from "./types";
 
-const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
 const JOB_QUERY_PARAMS = {
   publisher,
-  format: 'json',
-  v: '2',
+  format: "json",
+  v: "2",
   latlong: 1,
   radius: 10,
-  q: 'javascript', // just hardcoding this for now
+  q: "javascript" // just hardcoding this for now
 };
 
-const buildJobsUrl = (zip) => {
+const buildJobsUrl = zip => {
   const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
   return `${JOB_ROOT_URL}${query}`;
 };
@@ -30,7 +28,14 @@ export const fetchJobs = (region, callback) => async dispatch => {
     let { data } = await axios.get(url);
     dispatch({ type: FETCH_JOBS, payload: data });
     callback();
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
+};
+
+export const likeJob = job => {
+  return {
+    payload: job,
+    type: LIKE_JOB
+  };
 };
